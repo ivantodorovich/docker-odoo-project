f53e4d3f82d735abbd0c77a5811e11a0d2afe465
vrenaville <vincent.renaville@camptocamp.com>
2024-11-28 11:43:48 +0100

    fixup! fixup! fix: new flovar

---
+++ install/package_odoo_common_custom.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eo pipefail
+
+apt-get update -o Acquire::AllowInsecureRepositories=true
+apt-get install -y --no-install-recommends \
+ python3-html5lib \
+ python3-odf \
+ python3-pyinotify \
+ python3-simplejson \
+ python3-flake8 \
+ python3-pytest \
+ python3-pluggy \
+ python3-coverage \
+ python3-pytest-cov \
+ python3-argh \
+ python3-watchdog \
+ python3-atomicwrites \
+ python3-attrs \
+ python3-beautifulsoup4 \
+ python3-future \
+ python3-mccabe \
+ python3-more-itertools \
+ python3-pbr \
+ python3-pexpect \
+ python3-ptyprocess \
+ python3-py \
+ python3-pycodestyle \
+ python3-pyflakes \
+ python3-unicodecsv \
+ python3-wrapt \
+ python3-disutils 
